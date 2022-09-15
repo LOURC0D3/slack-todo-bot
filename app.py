285f1536156a1ab5d6ab6b50aab8b2b1545ba4a4
김찬인 <chanin@gimchan-in-ui-MacBookPro.local>
2022-09-16 01:17:31 +0900

    ADD: Get User Data

--- app.py
+++ app.py
@@ -9,5 +9,13 @@ class App(AsyncApp):
         super().__init__(token="xoxb-3924779483874-4020715904659-1ABd2ayLMGlIBxC4HPJefqlD",
                     signing_secret="6d5cbac7a0f8958c5e834d67356e8631")
         self.prisma = Prisma()
+    
+    async def get_user_by_slack_id(self, slack_id):
+        user = await self.prisma.user.find_unique(where={'slack_id': slack_id})
+
+        if user == None:
+            user = await self.prisma.user.create(data={'slack_id': slack_id})
+        
+        return user
 
 app = App()
